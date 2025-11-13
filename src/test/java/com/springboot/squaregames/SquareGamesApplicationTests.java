1af314af98ef52756b07fa234483e2cbd69e8571
Léa Deffaisse <lea.deffaisse@le-campus-numerique.fr>
2025-11-13 11:51:42 +0100

    first commit

---
+++ src/test/java/com/springboot/squaregames/SquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.squaregames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
